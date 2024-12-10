464c28c5e4d365865145b3817a804f64c9d6df4e
Alexander Yordanov <a.yordanow67@gmail.com>
2024-12-10 13:22:01 +0200

    Add ui for employee and device creation

--- src/pages/Devices.tsx
+++ src/pages/Devices.tsx
@@ -2,12 +2,16 @@ import Row from "../ui/Row.tsx";
 import Heading from "../ui/Heading.tsx";
 import DevicesTable from "../features/devices/DevicesTable.tsx";
 import Filter from "../ui/Filter.tsx";
+import Modal from "../ui/Modal.tsx";
+import Button from "../ui/Button.tsx";
+import CreateDevice from "../features/devices/CreateDevice.tsx";
 
 function Devices() {
   return (
-    <>
+    <Modal>
       <Row type="horizontal">
         <Heading as="h1">All devices</Heading>
+
         <Filter
           filterField="status"
           options={[
@@ -18,11 +22,24 @@ function Devices() {
           ]}
         />
       </Row>
-
+      <Modal.Open opens="createDevice">
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button
+            style={{
+              width: "fit-content",
+            }}
+          >
+            Add device
+          </Button>
+        </div>
+      </Modal.Open>
       <Row>
         <DevicesTable />
       </Row>
-    </>
+      <Modal.Window name="createDevice">
+        <CreateDevice />
+      </Modal.Window>
+    </Modal>
   );
 }
 
--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,18 +1,37 @@
 import Row from "../ui/Row.tsx";
 import Heading from "../ui/Heading.tsx";
 import EmployeesTable from "../features/employees/EmployeeTable.tsx";
+import Modal from "../ui/Modal.tsx";
+import Button from "../ui/Button.tsx";
+import CreateEmployee from "../features/employees/CreateEmployee.tsx";
 
 function Employees() {
   return (
-    <>
+    <Modal>
       <Row>
         <Heading as="h1">All Employees</Heading>
       </Row>
 
+      <Modal.Open opens="createEmployee">
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button
+            style={{
+              width: "fit-content",
+            }}
+          >
+            Add employee
+          </Button>
+        </div>
+      </Modal.Open>
+
       <Row>
         <EmployeesTable />
       </Row>
-    </>
+
+      <Modal.Window name="createEmployee">
+        <CreateEmployee />
+      </Modal.Window>
+    </Modal>
   );
 }
 
