c8dcf388a8333cddd7ef05a859d940b2b8a3db48
Alexander Yordanov <a.yordanow67@gmail.com>
2024-12-28 15:33:37 +0200

    Adjustment

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -2,11 +2,15 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm.tsx";
 import UpdateUserPasswordForm from "../features/authentication/UpdateUserPasswordForm.tsx";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 function Account() {
+  const { width } = useWindowSize();
   return (
     <>
-      <Heading as="h1">Update your account</Heading>
+      <Heading as={width !== null && width < 490 ? "h2" : "h1"}>
+        Update your account
+      </Heading>
 
       <Row>
         <Heading as="h3">Update user data</Heading>
