74aa50d80a41ee41cbe88d2a0cda479c726647ec
Alexander Yordanov <a.yordanow67@gmail.com>
2024-12-29 19:45:40 +0200

    Small adjustment for confirm delete window

--- src/ui/ConfirmDelete.tsx
+++ src/ui/ConfirmDelete.tsx
@@ -3,19 +3,22 @@ import Button from "./Button";
 import Heading from "./Heading";
 
 const StyledConfirmDelete = styled.div`
-  width: 40rem;
+  width: fit-content;
   display: flex;
   flex-direction: column;
   gap: 1.2rem;
+  justify-content: center;
 
   & p {
     color: var(--color-grey-500);
     margin-bottom: 1.2rem;
+    text-align: center;
   }
 
   & div {
     display: flex;
-    justify-content: flex-end;
+    justify-content: center;
+    align-items: center;
     gap: 1.2rem;
   }
 `;
@@ -33,10 +36,12 @@ function ConfirmDelete({
 }: ConfirmDeleteProps) {
   return (
     <StyledConfirmDelete>
-      <Heading as="h3">Delete {resourceName}</Heading>
+      <Heading as="h2" style={{ textAlign: "center" }}>
+        Delete {resourceName}
+      </Heading>
       <p>
-        Are you sure you want to delete this {resourceName} permanently? This
-        action cannot be undone.
+        Are you sure you want to delete this {resourceName} permanently? <br />
+        This action cannot be undone.
       </p>
 
       <div>
